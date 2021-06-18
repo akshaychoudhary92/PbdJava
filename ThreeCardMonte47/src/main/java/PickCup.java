5f8f3173418b6ad0c58ce15cdba6a56e6082a920
Akshay Choudhary <ak92choudhary@gmail.com>
2021-06-18 17:47:21 -0400

    created restart functionality for game

--- ThreeCardMonte47/src/main/java/PickCup.java
+++ ThreeCardMonte47/src/main/java/PickCup.java
@@ -2,25 +2,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class PickCup {
-    public void start(){
+    public void start() {
+
         Scanner s = new Scanner(System.in);
-        int playerAnwser = shuffle();
-
-
-
-        //I need 3 random numbers
-//            int x = rand.nextInt(4) + 1;
+        String input;
 
 
 
 
+            System.out.println("You slide up to Fast Eddie's card table and plop down your cash.\n" +
+                    "He glances at you out of the corner of his eye and starts shuffling.\n" +
+                    "He lays down three cards.");
 
-        System.out.println("You slide up to Fast Eddie's card table and plop down your cash.\n" +
-                "He glances at you out of the corner of his eye and starts shuffling.\n" +
-                "He lays down three cards.");
 
-
-        System.out.println("");
+            System.out.println("");
+do{
+    System.out.println("do you wanna play?");
+    input = s.nextLine();
+    if (input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yea")) {
 
         System.out.println("Which one is the Ace?");
 
@@ -29,54 +28,61 @@ public class PickCup {
         System.out.println("1  2  3");
 
 
-        Integer a = s.nextInt();
+        int a = Integer.parseInt(s.nextLine());
 
-        if(playerAnwser == a && a == 1){
-            System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
-            System.out.println("");
+    int playerAnwser = shuffle();
+    if (playerAnwser == a && a == 1) {
+        System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
+        System.out.println("");
+        System.out.println("AA ## ##");
+        System.out.println("AA ## ##");
+        System.out.println("1  2  3");
+    } else if (playerAnwser == a && a == 2) {
+        System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
+        System.out.println("");
+        System.out.println("## AA ##");
+        System.out.println("## AA ##");
+        System.out.println("1  2  3");
+    } else if (playerAnwser == a && a == 3) {
+        System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
+        System.out.println("");
+        System.out.println("## ## AA");
+        System.out.println("## ## AA");
+        System.out.println("1  2  3");
+    } else {
+        System.out.println("Ha! Fast Eddie wins again! The ace was card number " + playerAnwser);
+        if (playerAnwser == 1) {
             System.out.println("AA ## ##");
             System.out.println("AA ## ##");
             System.out.println("1  2  3");
-        }else if(playerAnwser == a && a == 2){
-            System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
-            System.out.println("");
+        } else if (playerAnwser == 2) {
             System.out.println("## AA ##");
             System.out.println("## AA ##");
             System.out.println("1  2  3");
-        }else if(playerAnwser == a && a == 3){
-            System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
-            System.out.println("");
+        } else {
             System.out.println("## ## AA");
             System.out.println("## ## AA");
             System.out.println("1  2  3");
-        }else{
-            System.out.println("Ha! Fast Eddie wins again! The ace was card number " + playerAnwser);
-            if(playerAnwser == 1){
-                System.out.println("AA ## ##");
-                System.out.println("AA ## ##");
-                System.out.println("1  2  3");
-            }else if(playerAnwser == 2){
-                System.out.println("## AA ##");
-                System.out.println("## AA ##");
-                System.out.println("1  2  3");
-            }else{
-                System.out.println("## ## AA");
-                System.out.println("## ## AA");
-                System.out.println("1  2  3");
-            }
         }
-
-
-
-
     }
 
-    public Integer shuffle(){
-        Random rand = new Random();
-        int x = rand.nextInt(3) + 1;
+    }else{
+        System.out.println("see ya");
+        break;
+    }
+        }while(true);
+    }
+
+
+
+
+
+
+        public Integer shuffle(){
+            Random rand = new Random();
+            int x = rand.nextInt(3) + 1;
 //        System.out.println(x);
-        return x;
-    }
-
+            return x;
+        }
 
 }
