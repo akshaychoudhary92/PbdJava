c068944411723748c1d766f26711548fb2ccdeb4
Akshay Choudhary <ak92choudhary@gmail.com>
2021-06-18 18:12:40 -0400

    created the code for fizzbuzz

--- fizzBuzz71/src/main/java/FizzBuzz.java
+++ fizzBuzz71/src/main/java/FizzBuzz.java
@@ -1,6 +1,34 @@
 public class FizzBuzz {
-    public void start(){
+
+    
+    public void start()
+    {
+        fizzBuzzOutput();
+    }
 
 
+
+
+
+
+
+
+
+    public String fizzBuzzOutput(){
+        String output = "";
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("fizzbuzz");
+
+            }else if(i % 5 == 0){
+                System.out.println("buzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+        return output;
     }
 }
