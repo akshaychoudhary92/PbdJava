02dd080e9949a5356df07dd0852ff0d6cdfdd8ad
Akshay Choudhary <ak92choudhary@gmail.com>
2021-06-21 15:21:59 -0400

    finished basic nested loops

---
+++ basicNestedLoops121/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        NestedLoops ns = new NestedLoops();
+        ns.start();
+    }
+}
---
+++ basicNestedLoops121/src/main/java/NestedLoops.java
@@ -0,0 +1,10 @@
+public class NestedLoops {
+    public void start(){
+        for (int i = 0; i <=5; i++){
+            for (int j = 0; j <= 5; j++){
+                System.out.println("(" + i + "," + j + ") ");
+            }
+        }
+        }
+    }
+
